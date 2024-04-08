6d0a138385d54a4b104668a115da161b2141f53b
mh70cz <mh70@mh70.cz>
2024-04-08 22:07:03 +0200

    .

---
+++ simple-math.js
@@ -0,0 +1,14 @@
+let x = 7;
+let y = 8;
+let z = 32;
+
+let a = x + y + z
+let b = z - x * y
+let c = (z - x) * y
+
+console.log('x =' ,x );
+console.log('y =' ,y );
+console.log('z =' ,z );
+console.log('x + y + z' ,a );
+console.log('z - x * y' ,b );
+console.log('(z - x) * y' ,c );
