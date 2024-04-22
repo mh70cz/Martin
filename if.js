742290a1f59a23a297e95d1c7f48967fbd79a6a4
mh70cz <mh70@mh70.cz>
2024-04-22 20:31:05 +0200

    .

---
+++ if.js
@@ -0,0 +1,76 @@
+
+let magicNumber = 7;
+
+console.log('Magic number is: ' + magicNumber);
+
+if (magicNumber === 7) {
+  console.log('The magic door opens!');
+}
+
+magicNumber = 8;
+
+console.log('Magic number is: ' + magicNumber);
+
+if (magicNumber === 7) {
+    console.log('The magic door opens!');
+  } else {
+    console.log('The magic door stays close!');
+  }
+
+
+
+let hero = "SuperMan";
+
+console.log("Hero is: " + hero);
+
+if (hero === "Spiderman") {
+  console.log("Let’s swing between buildings!");
+} else if (hero === "Batman") {
+  console.log("Time to drive the Batmobile!");
+} else if (hero === "Sonic") {
+  console.log("Run faaaaast!");
+} else if (hero === "Mario") {
+  console.log("Time to save Peach!");
+} else if (hero === "Martin") {
+  console.log("Lego heroes!");
+} else if (hero === "SuperMan") {
+  console.log("Time to fly!");
+  
+} else {
+  console.log("Do something else...");
+}
+
+
+
+
+
+
+let partOfDay = 'Mario time';   // morning , afternoon, evening
+
+console.log( 'Part of the day is: ' + partOfDay);
+
+if (partOfDay === 'morning') {
+  console.log('Time to go to school!');
+} else if (partOfDay === 'evening'){
+  console.log('Time to go to sleep!');
+} else {
+    console.log('Time to play!');
+}
+
+
+
+/*
+Part of day is: morning
+Time to go to school!
+
+
+Part of day is: evening
+Time to go to sleep!
+
+
+Part of day is: afternoon
+Time to play!
+
+
+ */
+
